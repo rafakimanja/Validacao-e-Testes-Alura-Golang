2a6059e38103af9e70d829334697093a613fe30d
Rafael Lopes Ens <rafaellopes.ifrs@gmail.com>
2024-10-08 10:19:33 -0300

    add validators

--- controllers/controller.go
+++ controllers/controller.go
@@ -28,6 +28,11 @@ func CriaNovoAluno(c *gin.Context) {
 			"error": err.Error()})
 		return
 	}
+	if err := models.ValidaDoadosDeAluno(&aluno); err != nil{
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
 	database.DB.Create(&aluno)
 	c.JSON(http.StatusOK, aluno)
 }
@@ -59,6 +64,12 @@ func EditaAluno(c *gin.Context) {
 	database.DB.First(&aluno, id)
 
 	if err := c.ShouldBindJSON(&aluno); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+
+	if err := models.ValidaDoadosDeAluno(&aluno); err != nil{
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": err.Error()})
 		return
